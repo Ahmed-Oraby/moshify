4e2405caef15446607bf44efc0b887869b47f0ca
Oraby96 <ahmed.oraby.1996@gmail.com>
2021-11-09 20:53:59 +0200

    Define the domain block styles

--- js/app.js
+++ js/app.js
@@ -1,7 +1,20 @@
 const collapsibles = document.querySelectorAll('.collapsible__header');
+const price = document.querySelector('.domain-block__prices');
 
 collapsibles.forEach(item => {
     item.addEventListener('click', () => {
         item.parentElement.classList.toggle('collapsible--expanded');
     });
+});
+
+price.addEventListener('click', (e) => {
+    if (e.target.localName === "span") {
+        for (let i = 0; i < price.children.length; i++) {
+            if (price.children[i].children[0].className === 'badge badge--secondary') {
+                price.children[i].children[0].className = 'badge';
+            }
+            e.target.classList.add('badge--secondary');
+        }
+
+    }
 });
